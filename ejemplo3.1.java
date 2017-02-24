785f52b3266a77d4a622b89fdf14c007050ff017
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 11:16:37 -0600

    con retorno sin parametros
    ejemplos de metodos en java

---
+++ ejemplo3.1.java
@@ -0,0 +1,15 @@
+package metodo3;
+
+public class ejemplo1 {
+public static void main(String args[]){
+int resultado;
+resultado= sumar();
+System.out.println("la suma es:"+resultado);
+}
+
+public static int sumar(){
+	int c=4, h=7;
+	int sumar=c+h;
+	return sumar;
+}
+}
